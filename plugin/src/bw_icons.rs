d38368d59a7b078c92e22b6a007ddabf4d438c79
SabrinaJewson <sejewson@gmail.com>
2022-07-22 21:11:39 +0100

    Remove completed TODO

--- plugin/src/bw_icons.rs
+++ plugin/src/bw_icons.rs
@@ -84,7 +84,6 @@ impl BwIcons {
     }
 
     pub fn get(&mut self, host: &str) -> Option<cairo::Surface> {
-        // TODO: fix the theme problem…
         let icon = self.icons.get_mut(host).unwrap();
 
         if let Icon::Waiting(handle) = icon {
